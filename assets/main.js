20e531528e4fe4b6f69c9ff65aaf352d2b44f2f1
macarena baltra <macarena.bc2011@gmail.com>
2017-07-07 21:54:54 -0400

    Aparecen pokes! - Pero no funciona la grilla

--- assets/main.js
+++ assets/main.js
@@ -11,5 +11,30 @@ $(document).ready(function(){
 	});
 
 
+	$.ajax({
+		url: 'http://pokeapi.co/api/v2/pokemon', // link donde estan todos los pokes
+		type: 'GET', // obtener datos
+		dataType: 'JSON', 
+		data: {'limit': '811'}, // hasta 811 pokes :S
+	})
+	.done(function(pok) {
+		mostrar(pok.results); // variable que recorrera los pokes
+		console.log(pok);
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error"); // para errores
+	})
+	.always(function() {
+		console.log("complete"); 
+	});
 	
+	var mostrar = function(data){ // recorrera los 811 pokes :S
+		data.forEach(function(elementPokes){
+			var nombre = elementPokes.name; // nombre pokemonsss
+
+			$('#pokesSec2').append("<img class='fotosPokemonss' src='http://img.pokemondb.net/artwork/" + nombre + ".jpg'>");
+		})
+	}
+
 });
