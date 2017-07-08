6d2444cc2a5da0ed46b6e24ce080aea2195c71b5
macarena baltra <macarena.bc2011@gmail.com>
2017-07-07 22:37:11 -0400

    Muriendo - Ensalada 3 + aderezo de nombre

--- assets/main.js
+++ assets/main.js
@@ -32,10 +32,10 @@ $(document).ready(function(){
 	var mostrar = function(data){ // recorrera los 811 pokes :S
 		data.forEach(function(elementPokes){
 			var nombre = elementPokes.name; // nombre pokemonsss
-
+			// Fotos
 			$('#pokesSec2').append("<div id='pokeSecFotos' class='col-md-2'><div class='row'><img class='fotosPokemonss img-responsive' src='http://img.pokemondb.net/artwork/" + nombre + ".jpg'></div></div>");
-
-
+			// Nombre de pokes
+			$('#pokeSecFotos').append("<h2>" + nombre + "</h2>");
 		})
 	}
 
